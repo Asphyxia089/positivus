721d3d65e567db1d0d88df18a3a2e45bd9907540
Asphyxia089 <perow.maksim2015@gmail.com>
2024-11-23 17:43:52 +0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,23 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
 				test: /\.html$/i,
 				loader: 'html-loader',
 			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
 			},
 		],
 	},
